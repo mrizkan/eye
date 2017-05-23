f0ec333c263490ad5b52fe76e2b756b1c2c9be46
mrizkan <rizkan@itmartx.net>
2017-05-23 15:14:16 +0530

    updated log screen with image

--- application/views/inc/top_bar.php
+++ application/views/inc/top_bar.php
@@ -18,6 +18,40 @@
 
         </ul>
 
+        <ul class="nav navbar-nav pull-right">
+            <li class="nav-item dropdown notification-list">
+                <a class="nav-link dropdown-toggle arrow-none waves-effect waves-light nav-user" data-toggle="dropdown" href="#" role="button"
+                   aria-haspopup="false" aria-expanded="false">
+                    <img src="<?php echo base_url(); ?>images/admin.png" alt="user" class="img-circle">
+                </a>
+                <div class="dropdown-menu dropdown-menu-right dropdown-arrow profile-dropdown " aria-labelledby="Preview">
+                    <!-- item-->
+                    <div class="dropdown-item noti-title">
+                        <h5 class="text-overflow"><small>Welcome ! Rizwan</small> </h5>
+                    </div>
+
+                    <!-- item-->
+
+
+                    <!-- item-->
+                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                        <i class="zmdi zmdi-settings"></i> <span>Settings</span>
+                    </a>
+
+                    <!-- item-->
+
+
+                    <!-- item-->
+                    <a href="<?php echo base_url('Customer/index'); ?>" class="dropdown-item notify-item">
+                        <i class="zmdi zmdi-power"></i> <span>Logout</span>
+                    </a>
+
+                </div>
+            </li>
+
+        </ul>
+
+
     </nav>
 
 </div>
